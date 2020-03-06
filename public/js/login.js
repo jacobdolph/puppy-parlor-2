da87a92badfd52b2d366d67b54a5325e2063335a
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-03-05 20:18:54 -0700

    created login and signup js pages

---
+++ public/js/login.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    var loginForm = $("form.login");
+    var emailVal = $("input.email");
+    var passwordVal = $("input.password");
+
+
+    loginForm.on("submit", function (event) {
+        event.preventDefault();
+        var userData = {
+            email: emailVal.val().trim(),
+            password: passwordVal.val().trim()
+        };
+
+        if (!userData.email || !userData.password) {
+            return;
+        }
+        loginUser(userData.email, userData.password);
+        email.val("");
+        password.val("");
+    });
+    function loginUser(email, password) {
+        $.post("/api/login", {
+            email: email,
+            password: password
+        })
+            .then(function () {
+                window.location.replace("/members.html");
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }
+});
