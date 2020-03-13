30fd894da34cca85adb2500626a98d6a7895e508
Oaklee Ohmie <oakleeohmie@outlook.com>
2020-03-12 20:20:03 -0600

    displayed the room names in html

--- public/js/booking.js
+++ public/js/booking.js
@@ -5,8 +5,14 @@ $(document).ready(function () {
         method: "GET"
     }).then(function (pageInfo) {
         console.log(pageInfo);
-        for (i = 0; i < pageInfo.length; i++) {
-            $('#rooms').append($('<div/>', { id: i, 'class': 'card' }))
+        for (i = 0; i < pageInfo.length + 1; i++) {
+            // $('#rooms').append($('<div/>').attr("id", [i]));
+            $("#rooms").append($roomsList);
+            console.log(pageInfo[i].room_name);
+            var $roomsList = $('<div/>').addClass("col").attr("id", [i]);
+            var $roomListItems = $("<li class='list-group-item'>");
+            $roomListItems.append("<h5>" + pageInfo[i].room_name + "</h5>");
+            $roomsList.append($roomListItems);
         };
     });
     if (this.room_empty === true)
