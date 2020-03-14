a26478797472b495886d01e50a8f2246c83e84e9
Jacob Dolph <jacob.dolph702@gmail.com>
2020-03-14 12:03:44 -0600

    comenting out unused dbs

--- db/seed.sql
+++ db/seed.sql
@@ -1,68 +1,68 @@
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("John", "Doe", "johnDoe@gmail.com", "111111", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Jerry", "Dimmer", "jerryDoe@gmail.com", "111112", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Joey", "Summers", "joeyDoe@gmail.com", "111113", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Boe", "Duke", "boeDoe@gmail.com", "111114", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("todd", "Randall", "toddDoe@gmail.com", "141111", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Jeremy", "Conner", "JeremyDoe@gmail.com", "511111", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Rocky", "Breatie", "rockyDoe@gmail.com", "611111", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Dawson", "Copeland", "DawsonDoe@gmail.com", "111711", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Milicent", "Grant", "MilicentDoe@gmail.com", "111181", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Sally", "Wachowski", "sallyDoe@gmail.com", "1111911", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Gretchen", "Linus", "GretchenDoe@gmail.com", "111881", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Terry", "Cruise", "terryDoe@gmail.com", "111771", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Mal", "Fran", "malDoe@gmail.com", "111551", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Luke", "Skywalker", "lukeDoe@gmail.com", "441111", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Don", "Trudee", "donDoe@gmail.com", "111133", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Chris", "Tomlin", "chrisDoe@gmail.com", "112211", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO users
+INSERT INTO Users
     (first_name, last_name, email, password, createdAt, updatedAt)
 VALUES
     ("Pat", "Lacosta", "patDoe@gmail.com", "1113331", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
@@ -72,164 +72,150 @@ INSERT INTO dogactivities
     (trailDistance, feedFrequency, feedAmount, createdAt, updatedAt)
 VALUES
     ("5", "Twice a Day", "One Cup Per Day", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-<<<<<<< HEAD
 INSERT INTO dogactivities
     (trailDistance, feedFrequency, feedAmount, createdAt, updatedAt)
 VALUES
     ("10", "Twice a Day", "Two Cups Per Day", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
 INSERT INTO dogactivities
     (trailDistance, feedFrequency, feedAmount, createdAt, updatedAt)
-=======
-INSERT INTO dogactivities
-    (trailDistance, feedFrequency, feedAmount,, createdAt, updatedAt)
-VALUES
-    ("10", "Twice a Day", "Two Cups Per Day", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-INSERT INTO dogactivities
-    (trailDistance, feedFrequency, feedAmount, , createdAt, updatedAt)
->>>>>>> 2b8bd82221c658dd5c93b55af667cb2969096e0d
 VALUES
 ("15", "Twice a Day", "Three and a Half Cups Per Day", "2020-01-01 10:10:10", "2020-01-01 10:10:10");
-
--- seed for dogs
-INSERT INTO dogs
+-- seed for Dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
-<<<<<<< HEAD
 VALUES
 ("Romeo", "Small", "Chihuahua", True,"2020-01-01 10:10:10", "2020-01-01 10:10:10",1,1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Todd", "Medium", "Lab", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 2, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Raskal", "Large", "Mastif", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 3, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Sir Kingsly", "Medium", "Beagle", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 4, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Spot", "Small", "Chihuahua", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 5, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Fido", "Medium", "Dalmation", False, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 6, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Rex", "Large", "Great Dane", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 7, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Sally", "Medium", "Poodle", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 8, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Mister Tomkins", "Small", "Terrier", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 9, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Coco", "Medium", "Beagle", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 10, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Daisy", "Large", "Great Dane", False, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 11, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Rosie", "Medium", "Bull Dog", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 12, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Teddy", "Small", "Chihuahua", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 13, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Milo", "Medium", "", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 14, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Frankie", "Small", "Chihuahua", False, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 15, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Lola", "Large", "Saint Bernard", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 16, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Oscar", "Small", "Chihuahua", True, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 17, 1)
-=======
+
 VALUES
 ("Romeo", "Small", "Chihuahua", true,"2020-01-01 10:10:10", "2020-01-01 10:10:10",1,1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Todd", "Medium", "Lab", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 2, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Raskal", "Large", "Mastif", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 3, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Sir Kingsly", "Medium", "Beagle", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 4, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Spot", "Small", "Chihuahua", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 5, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Fido", "Medium", "Dalmation", false, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 6, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Rex", "Large", "Great Dane", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 7, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Sally", "Medium", "Poodle", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 8, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Mister Tomkins", "Small", "Terrier", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 9, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Coco", "Medium", "Beagle", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 10, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Daisy", "Large", "Great Dane", false, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 11, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Rosie", "Medium", "Bull Dog", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 12, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Teddy", "Small", "Chihuahua", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 13, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Milo", "Medium", "Beagle", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 14, 2);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Frankie", "Small", "Chihuahua", false, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 15, 1);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Lola", "Large", "Saint Bernard", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 16, 3);
-INSERT INTO dogs
+INSERT INTO Dogs
     (name, size, breed, vaccinated, createdAt, updatedAt, UserId, dogActivityId)
 VALUES
     ("Oscar", "Small", "Chihuahua", true, "2020-01-01 10:10:10", "2020-01-01 10:10:10", 17, 1);
-<<<<<<< HEAD
->>>>>>> 2b8bd82221c658dd5c93b55af667cb2969096e0d
-=======
 
 -- rooms seeds
 
@@ -262,4 +248,4 @@ INSERT INTO rooms
     (room_name, room_empty, createdAt, updatedAt)
 VALUES
     ("Standard", true, '2020-01-01 10:10:10', '2020-01-01 10:10:10');
->>>>>>> 171cee77ed1baa606055b61924a3b025dedc608e
+
--- models/Dog.js
+++ models/Dog.js
@@ -1,24 +1,24 @@
-module.exports = function (sequelize, DataTypes) {
-    var Dog = sequelize.define("Dog", {
-        name: DataTypes.STRING,
-        size: DataTypes.STRING,
-        breed: DataTypes.STRING,
-        vaccinated: DataTypes.BOOLEAN,
-    });
-    Dog.associate = function (models) {
-        Dog.belongsTo(models.User, {
-            foreignKey: {
-                allowNull: false
-            }
-        }),
+// module.exports = function (sequelize, DataTypes) {
+//     var Dog = sequelize.define("Dog", {
+//         name: DataTypes.STRING,
+//         size: DataTypes.STRING,
+//         breed: DataTypes.STRING,
+//         vaccinated: DataTypes.BOOLEAN,
+//     });
+//     Dog.associate = function (models) {
+//         Dog.belongsTo(models.User, {
+//             foreignKey: {
+//                 allowNull: false
+//             }
+//         }),
 
-            Dog.hasOne(models.rooms, {
-                foreignKey: {
-                    allowNull: true
-                }
-            });
-    };
+//             Dog.hasOne(models.rooms, {
+//                 foreignKey: {
+//                     allowNull: true
+//                 }
+//             });
+//     };
 
-    return Dog;
-};
+//     return Dog;
+// };
 
