b4cfbb3a21939d29fcee67e5aefb4040866355bb
Jacob Dolph <jacob.dolph702@gmail.com>
2020-03-14 08:36:17 -0600

    changed operator alias to make this thing work hopefully

--- config/config.js
+++ config/config.js
@@ -4,20 +4,18 @@ module.exports = {
     password: process.env.MYSQL_PASSWORD,
     database: "puppy_parlor_db",
     host: "localhost",
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: "mysql"
   },
   test: {
     username: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     database: "puppy_parlor_db_test",
     host: "localhost",
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: "mysql"
   },
   production: {
     use_env_variable: "JAWSDB_URL",
-    dialect: "mysql",
-    operatorsAliases: false
+    dialect: "mysql"
+
   }
 }
